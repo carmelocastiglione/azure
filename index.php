da529bc387eba449f87edc41b1bb87115c58b37a
Carmelo Castiglione <carmelo.c.castiglione@gmail.com>
2024-03-13 11:40:11 +0100

    Testing environment

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-    echo "Hello world";
-    echo "Testing PHP";
-?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing PHP</title>
+</head>
+<body>
+    <p><?php echo "Hello world"; ?></p>
+    <p><?php echo env('AZURE_TEST'); ?></p>
+</body>
+</html>
