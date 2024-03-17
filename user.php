a0108671e24519513edccfbd2e4606126b8412c0
Carmelo Castiglione <carmelo.c.castiglione@gmail.com>
2024-03-17 19:10:29 +0100

    user.php

--- user.php
+++ user.php
@@ -1,10 +1,3 @@
 <?php
-    $parts = parse_url($url);
-    parse_str($parts['query'], $query);
-    userCode = $query['userCode'];
-    if (userCode == 1) {
-        echo "ok";
-    } else {
-        echo "not found";
-    }
+    echo "ok";
 ?>
