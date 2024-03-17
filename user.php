ec1b049b28be2bfa5aa96b0ac5d6f393cb792bed
Carmelo Castiglione <carmelo.c.castiglione@gmail.com>
2024-03-17 19:18:49 +0100

    user.php

--- user.php
+++ user.php
@@ -1,11 +1,4 @@
 <?php
     $url = $_SERVER[REQUEST_URI];
-    $parts = parse_url($url);
-    parse_str($parts['query'], $query);
-    user = $query['user'];
-    if (user == 1) {
-        echo "ok";
-    } else {
-        echo "not found";
-    }
+    echo $url;
 ?>
