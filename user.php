21e5f58b332aab4b59503a0ab70a30fc05bc9634
Carmelo Castiglione <carmelo.c.castiglione@gmail.com>
2024-03-26 21:35:49 +0100

    Fixed user.php

--- user.php
+++ user.php
@@ -1,8 +1,27 @@
-<?php
-    $code = $_GET['code'];
-    if ($code == 1) {
-        echo "ok";
-    } else {
-        echo "not found";
+<?php 
+include ".env";
+
+$code = $_GET["code"];
+
+$connectionInfo = array(
+  "UID" => "" . getenv("AZURE_DB_USER") . "", 
+  "pwd" => "" . getenv("AZURE_DB_PASS") . "", 
+  "Database" => "" . getenv("AZURE_DB_NAME") . "", 
+  "LoginTimeout" => 30, 
+  "Encrypt" => 1, 
+  "TrustServerCertificate" => 0);
+$serverName = "" . getenv("AZURE_DB_SERVER") . "," . getenv("AZURE_DB_PORT") . "";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+if ($conn) {
+    $sql = "SELECT id, name, code FROM users WHERE code='$code'";
+    if (($result = sqlsrv_query($conn, $sql)) === false) {
+        die (print_r(sqlsrv_errors(), true));
     }
+    $obj = sqlsrv_fetch_object( $result );
+    if ($obj !== null) {
+        echo 'ok';
+    } else {
+        echo 'not found';
+    }
+}
 ?>
